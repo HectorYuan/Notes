655f9ad7215dd7a6e3dc3d438ff7c3c625776ef0
yeonix <yuanyedmc@hotmail.com>
2020-08-27 21:58:39 +0800

    Modified

---
+++ filesum.py
@@ -0,0 +1,26 @@
+#%%
+import re
+from os import listdir
+from os.path import isdir, isfile
+
+def get_file_context():
+    pattern = r'(.*)?.md'
+    context_dict = {}
+    root_path = listdir()
+    for path in root_path:
+        if isdir(path) and path != '.git':
+            files = listdir(path)
+            context_dict[path] = {re.search(pattern, file).group(0):f'{path}/{file}' for file in files if re.search(pattern, file)}
+    
+    return context_dict
+
+def to_md(dict):
+    with open('SUMMARY.md', 'w') as f:
+        f.write('# Summary\n')
+        for key, value in dict.items():
+            f.write(f'\n## {key}\n')
+            for key_2, value_2 in value.items():
+                f.write(f'* [{key_2}]({value_2})\n')
+
+dict = get_file_context()
+to_md(dict)
